0497a4aa06b5d7c09e59764c4971c707eb762f0e
iavtar <indra.avatarsingh@gmail.com>
2020-07-17 22:32:44 +0300

    added check for delete by user approval

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -38,10 +38,13 @@ export const getProject = (id, history) => async dispatch => {
 };
 
 export const deleteProject = (id) => async dispatch => {
-    await axios.delete(`/api/project/${id}`)
-   
-    dispatch({
-        type: DELETE_PROJECT,
-        payload: id
-    });
+    if (window.confirm("Are you sure? This will delete the project and all the data related to it")) {
+        await axios.delete(`/api/project/${id}`)
+
+        dispatch({
+            type: DELETE_PROJECT,
+            payload: id
+        });
+    }
+
 };
