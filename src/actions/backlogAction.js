034cefd29ddf6da916efc927399d539cd78b6463
iavtar <indra.avatarsingh@gmail.com>
2020-07-20 01:28:16 +0300

    added client side validation for project summary

--- src/actions/backlogAction.js
+++ src/actions/backlogAction.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
-import { GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from './types';
+import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from './types';
 
 export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
     try {
         await axios.post(`/api/backlog/${backlog_id}`, project_task);
         history.push(`/projectBoard/${backlog_id}`);
-    } catch (error) {
-        
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
     }
 }
