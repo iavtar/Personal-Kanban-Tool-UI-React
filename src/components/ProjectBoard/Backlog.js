3a723499f72d785912c5c76cdc7d65f4d142fb19
iavtar <indra.avatarsingh@gmail.com>
2020-07-21 00:08:10 +0300

    added code for organizing project task by status and priority

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -8,7 +8,25 @@ class Backlog extends Component {
 
         const tasks = project_tasks_prop.map(project_task =>(
             <ProjectTask key={project_task.id} project_task = {project_task} />
-        ))
+        ));
+
+        let todoItems = [];
+        let inProgress = [];
+        let doneItems = [];
+
+        for(let i = 0; i < tasks.length; i++){
+            if(tasks[i].props.project_task.status === "TO_DO"){
+                todoItems.push(tasks[i]);
+            }
+
+            if(tasks[i].props.project_task.status === "IN_PROGRESS"){
+                inProgress.push(tasks[i]);
+            }
+
+            if(tasks[i].props.project_task.status === "DONE"){
+                doneItems.push(tasks[i]);
+            }
+        }
 
         return (
             <div>
@@ -21,10 +39,7 @@ class Backlog extends Component {
                                     <h3>TO DO</h3>
                                 </div>
                             </div>
-                            {tasks}
-                           {
-                               //insert task here
-                           }
+                            {todoItems}
                         </div>
 
 
@@ -34,9 +49,7 @@ class Backlog extends Component {
                                     <h3>In Progress</h3>
                                 </div>
                             </div>
-                            {/* {< !--SAMPLE PROJECT TASK STARTS HERE -->} */}
-
-                            {/* {< !--SAMPLE PROJECT TASK ENDS HERE -->} */}
+                            {inProgress}
                         </div>
 
 
@@ -46,9 +59,7 @@ class Backlog extends Component {
                                     <h3>Done</h3>
                                 </div>
                             </div>
-                            {/* { < !--SAMPLE PROJECT TASK STARTS HERE -->} */}
-
-                            {/* {< !--SAMPLE PROJECT TASK ENDS HERE -->} */}
+                            {doneItems}
                         </div>
                     </div>
                 </div>
